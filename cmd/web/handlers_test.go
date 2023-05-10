cc64649c81694da5f9fcbbb48a15cceffed349c2
Shorya Pratap Singh <shouryapschauhan@gmail.com>
2023-05-10 13:19:41 +0530

    add test Homepage for session

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -36,4 +39,40 @@ func Test_application_handlers(t *testing.T) {
 		}
 
 	}
+}
+
+func Test_App_Home(t *testing.T) {
+	//create a request
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	req = addContextAndSessionToRequest(req, app)
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(app.Home)
+
+	handler.ServeHTTP(rr, req)
+
+	//check status code
+	if rr.Code != http.StatusOK {
+		t.Errorf("Test_App_Home returned wrong status code; expected 200 but got %d", rr.Code)
+	}
+
+	body, _ := io.ReadAll(rr.Body)
+	if !strings.Contains(string(body), `<small> From Session:`){
+		t.Error("Test_App_Home did not find correct text in HTML")
+	}
+}
+
+func getCtx(req *http.Request) context.Context {
+	ctx := context.WithValue(req.Context(), contextUserKey, "unknown")
+	return ctx
+}
+
+func addContextAndSessionToRequest(req *http.Request, app application) *http.Request {
+	req = req.WithContext(getCtx(req))
+
+	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
+
+	return req.WithContext(ctx)
 }
