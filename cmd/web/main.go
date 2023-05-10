80f7a3f7700abc4f2f9c6be23be9006d0a114934
Shorya Pratap Singh <shouryapschauhan@gmail.com>
2023-05-10 11:59:33 +0530

    Add session to the webapp

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/alexedwards/scs/v2"
 )
 
-type application struct{}
+type application struct {
+	Session *scs.SessionManager
+}
 
 func main() {
 	// set up an app config
@@ -13,6 +17,9 @@ func main() {
 
 	//get application routes
 	mux := app.routes()
+
+	//get a session manager
+	app.Session = getSession()
 
 	//print out a message
 	log.Println("Starting server on port 8080...")
